ddc6f370757a7cf78764021c246f449a73378e1d
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2020-11-25 23:50:11 -0300

    up mbox

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:mobx/mobx.dart';
+import 'package:todomobx/stores/login_store.dart';
 import 'package:todomobx/widgets/custom_icon_button.dart';
 import 'package:todomobx/widgets/custom_text_field.dart';
 
@@ -10,6 +13,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  LoginStore loginStore = LoginStore();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -31,44 +36,57 @@ class _LoginScreenState extends State<LoginScreen> {
                       hint: 'E-mail',
                       prefix: Icon(Icons.account_circle),
                       textInputType: TextInputType.emailAddress,
-                      onChanged: (email) {},
+                      onChanged: loginStore.setEmail,
                       enabled: true,
                     ),
                     const SizedBox(
                       height: 16,
                     ),
-                    CustomTextField(
-                      hint: 'Senha',
-                      prefix: Icon(Icons.lock),
-                      obscure: true,
-                      onChanged: (pass) {},
-                      enabled: true,
-                      suffix: CustomIconButton(
-                        radius: 32,
-                        iconData: Icons.visibility,
-                        onTap: () {},
-                      ),
+                    Observer(
+                      builder: (_) {
+                        return CustomTextField(
+                          hint: 'Senha',
+                          prefix: Icon(Icons.lock),
+                          obscure: !loginStore.passwordVisible,
+                          onChanged: loginStore.setPassword,
+                          enabled: true,
+                          suffix: CustomIconButton(
+                            radius: 32,
+                            iconData: loginStore.passwordVisible
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                            onTap: loginStore.togglePasswordVisibility,
+                          ),
+                        );
+                      },
                     ),
                     const SizedBox(
                       height: 16,
                     ),
-                    SizedBox(
-                      height: 44,
-                      child: RaisedButton(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(32),
-                        ),
-                        child: Text('Login'),
-                        color: Theme.of(context).primaryColor,
-                        disabledColor:
-                            Theme.of(context).primaryColor.withAlpha(100),
-                        textColor: Colors.white,
-                        onPressed: () {
-                          Navigator.of(context).pushReplacement(
-                              MaterialPageRoute(
-                                  builder: (context) => ListScreen()));
-                        },
-                      ),
+                    Observer(
+                      builder: (_) {
+                        return SizedBox(
+                          height: 44,
+                          child: RaisedButton(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(32),
+                            ),
+                            child: Text('Login'),
+                            color: Theme.of(context).primaryColor,
+                            disabledColor:
+                                Theme.of(context).primaryColor.withAlpha(100),
+                            textColor: Colors.white,
+                            onPressed: loginStore.isFormValid
+                                ? () {
+                                    Navigator.of(context).pushReplacement(
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                ListScreen()));
+                                  }
+                                : null,
+                          ),
+                        );
+                      },
                     )
                   ],
                 ),
--- lib/stores/login_store.dart
+++ lib/stores/login_store.dart
@@ -1,1 +1,48 @@
 import 'package:mobx/mobx.dart';
+
+part 'login_store.g.dart';
+
+class LoginStore = _LoginStore with _$LoginStore;
+
+abstract class _LoginStore with Store {
+  //constructor
+  _LoginStore() {
+    autorun((_) {
+      print(isFormValid);
+    });
+  }
+
+  @observable
+  String email = "";
+
+  //setar email
+  @action
+  void setEmail(String value) => email = value;
+
+  @observable
+  String password = "";
+
+  //mostra a senha com icone visble
+  @observable
+  bool passwordVisible = false;
+
+  @action
+  void togglePasswordVisibility() => passwordVisible = !passwordVisible;
+
+  @action
+  void setPassword(String value) => password = value;
+
+  //validação dos campos
+  @computed
+  bool get isEmailValid => RegExp(
+          r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+      .hasMatch(email);
+
+  @computed
+  bool get isPasswordValid => password.length >= 6;
+
+  @computed
+  bool get isFormValid => isEmailValid && isPasswordValid;
+}
+
+//flutter packages pub run build_runner watch
